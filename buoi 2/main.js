0e7b46bd444bfc47f7957a61c05efbfca7d95814
GodOfClown2k8 <vonguyenminhkhoi1007@gmail.com>
2023-08-17 19:40:45 +0700

    buoi 2

---
+++ buoi 2/main.js
@@ -0,0 +1,40 @@
+// let MyNumber = 1;
+// while (MyNumber <= 20) {
+//     console.log(MyNumber);
+//     MyNumber++;
+// }
+
+
+// let Number = 0;
+
+// do {
+//      console.log(Number);  
+//      Number++;
+    
+// } while (Number < 21);
+
+// let Number = 0;
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+    
+// }
+// let MyNumberArray = [1,2,3,4];
+// let MyArray = ['grape','banana','apple','peach','cocomelon'];
+// for (let i = 0; i < 5; i++) {
+//    console.log(MyArray[i]);
+    
+// }
+// let i = 0
+
+// while (i<5) {
+//     console.log(MyArray[i]);
+//     i++;
+// }
+// do {
+//     console.log(MyArray[i]);
+//     i++;
+// } while (i<5);
+// console.log(MyNumberArray[1]);
+// console.log(MyNumberArray[3]);
+
+
