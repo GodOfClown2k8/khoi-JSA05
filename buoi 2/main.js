2bb2aa3f81503d38b8c02dffdefe8abc6505cde4
GodOfClown2k8 <vonguyenminhkhoi1007@gmail.com>
2023-08-20 12:08:31 +0700

    buoi 3

--- buoi 2/main.js
+++ buoi 2/main.js
@@ -1,40 +1,60 @@
-// let MyNumber = 1;
-// while (MyNumber <= 20) {
-//     console.log(MyNumber);
-//     MyNumber++;
+let MyArray = [4, 5, 7, 8, 9, 1];
+console.log(MyArray);
+// // for (let i = 0; i < array.length; i++) {
+// //    console.log("Giá trị của phần tử thứ",i,"là :",MyArray);
+
 // }
+// MyArray[2] = 6;
+// console.log(MyArray);
 
+// MyArray.splice(2,1,7,8); [1]:vị trí bắt đầu cắt 
+                             
 
-// let Number = 0;
+// console.log(MyArray);
 
-// do {
-//      console.log(Number);  
-//      Number++;
-    
-// } while (Number < 21);
+// console.log(MyArray.length);độ dài mảng
 
-// let Number = 0;
-// for (let i = 0; i < 10; i++) {
-//     console.log(i);
-    
+// MyArray.push(7) xóa phần tử cuối mảng
+// console.log(MyArray);
+
+// MyArray.pop() xóa số cuối
+// console.log(MyArray);
+
+// MyArray.unshift(9) thêm phần tử đầu
+// console.log(MyArray);
+
+// MyArray.shift() xóa phần tử đầu tiên
+// console.log(MyArray);
+
+// let dog = {
+//     name: 'Alaska',
+//     age: 4
 // }
-// let MyNumberArray = [1,2,3,4];
-let MyArray = ['grape','banana','apple','peach','cocomelon'];
-for (let i = 0; i < 5; i++) {
-   console.log(MyArray[i]);
-    
-}
-// let i = 0
+// dog.name = 'mmb'
+// dog.action = 'Bark'
+// delete (dog.action)
+// console.log(dog);
 
-// while (i<5) {
-//     console.log(MyArray[i]);
-//     i++;
+// let cat = new Object({
+//     name: 'tom',
+//     age: 3,
+//     enemy: {
+//         name: 'nah guys',
+//         age: 8
+//     }
+// })
+// console.log(cat);
+
+// let dog1 = ["alaska",6]
+// let dog2 = ["nvm",5]
+// let dog3 = [dog1,dog2]
+// console.log(dog3);
+// console.log(dog3[1][0]);
+
+// let Giotraicay = [
+//   ["xoai", 2],
+//   ["dua hau", 3],
+// ];
+// for (let i = 0; i < Giotraicay.length; i++) {
+//   console.log("Giỏ trái cây có:" + Giotraicay[i][0] + Giotraicay[i][1]);
 // }
-// do {
-//     console.log(MyArray[i]);
-//     i++;
-// } while (i<5);
-// console.log(MyNumberArray[1]);
-// console.log(MyNumberArray[3]);
-
-
---
+++ buoi3/main.js
@@ -0,0 +1,32 @@
+
+// For in cho mảng
+// let arr = ["toán","ngữ văn","Tiếng anh"]
+
+
+// for (let i in arr) {
+//     {
+//         console.log(arr);
+        
+//     }
+// }
+
+// for in cho object
+// let object = {
+//     'name' : "Nguyễn Văn Đẩu",
+//     'sex' : "Không xác định",
+//     'crime' : "Vi phạm hành chính"
+// }
+// for (const i in object) {
+//      {
+//         console.log(object[i]);
+        
+//     }
+// }
+
+
+// for of cho mảng
+let arr = ["toán","ngữ văn","Tiếng anh"]
+for (let value of arr) {
+    console.log(value);
+}
+// for of cho obj ko đc
